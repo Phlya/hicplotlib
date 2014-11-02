40a5ae229c14b1f1534e9c97b1329d8ccf6dd1c6
Ilya Flyamer <flyamer@gmail.com>
2014-11-02 17:18:19 +0100

    Variuos updates

--- hicplotlib.py
+++ hicplotlib.py
@@ -5,14 +5,17 @@ Created on Tue Apr 29 17:34:20 2014
 @author: ilya
 """
 from os import path
+#from matplotlib.colors import Normalize
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mticker
 import matplotlib.colors as mcolors
+import matplotlib.cm as cmap
 from mpl_toolkits.axes_grid1 import host_subplot#, make_axes_locatable
+#from matplotlib.ticker import MultipleLocator
 #from math import ceil
 #from smooth import smooth
 import numpy as np
-#from scipy import ndimage
+from scipy import ndimage
 
 #import track
 
@@ -20,22 +23,29 @@ class HiCPlot:
     def __init__(self):
         self.name = ''
         self.name2 = ''
-        self.cmap='jet'
+        self.cmap=cmap.get_cmap()
         self.data=None
         self.resolution=1
         self.chromosomes = None
         self.boundaries = None
-        plt.ion()
+        self.data_dict = {}
+#        plt.ion()
     def read_matrix(self, matrix_file, name=''):
         self.data = np.loadtxt(matrix_file)
         if name is not None:
             self.name = name
-    def read_and_combine_two_matrices(self, matrixfile1, matrixfile2, 
-                                      names=None):
-        self.data = np.triu(np.loadtxt(matrixfile1), 1) + \
-                    np.tril(np.loadtxt(matrixfile2), 1)
+            self.data_dict[name]=self.data
+    def read_two_matrices(self, matrix_files, names=None):
+        self.data, self.data2 = np.loadtxt(matrix_files[0]), \
+                                np.loadtxt(matrix_files[1])
         if names is not None:
             self.name, self.name2 = names
+            self.data_dict[self.name]=self.data
+            self.data_dict[self.name2]=self.data2
+            
+    def get_data(self, name):
+        return self.data_dict[name]
+        
     def set_chromosomes(self, chromosomes):
         self.chromosomes = chromosomes
     def set_chromosomes_boundaries(self, boundaries):
@@ -87,18 +97,18 @@ class HiCPlot:
                                             str(int(value))[-3:-1] == '00' \
         else '%1.0f' % value
 
-    def _get_title(self):
+    def _make_title(self):
         if self.name and self.name2:
-            return self.name2+'/'+self.name
+            return self.name+'/'+self.name2
         elif self.name:
             return self.name
         else:
             return ''
             
-    def _make_axlabels(self):
+    def _set_axlabels(self):
         if self.name and self.name2:
-            self.ax.set_xlabel(self.name, size=20)
-            self.ax.set_ylabel(self.name2, size=20)
+            self.ax.set_ylabel(self.name, size=15)            
+            self.ax.set_xlabel(self.name2, size=15)            
         else:
             return
     
@@ -118,18 +128,121 @@ class HiCPlot:
 
     def clear_figure(self):
         plt.clf()
-    
-    def plot_whole_genome_heatmap(self, data=None, savepath=False, 
-                                  format='svg'):
+
+    def hide_bads(self, data=None, mask_zeroes=True, substitute = 'min', 
+                  cmap=None, col='w', alpha=0.0):
         if data is None:
             data=self.data
-        self.locations = [np.mean(i)*self.resolution for i in self.boundaries]
+        if cmap is None:
+            cmap=self.cmap
+        data = np.ma.masked_where(np.isneginf(data), data)
+        data = np.ma.masked_where(np.isinf(data), data)
+        if substitute == 'min':
+            substitute = np.min(data)
+#        data[np.isinf(data)] = substitute
+#        data[np.isneginf(data)] = substitute
+
+        if mask_zeroes:
+            data = np.ma.masked_equal(data, 0)
+        else:
+            data[data == 0] = substitute
+        data = np.ma.array (data, mask=np.isnan(data))
+        cmap.set_bad(col, alpha)
+#        apply_cmap()
+        return data
+    
+    def _plot_combined_heatmap(self, data, data2, *args, **kwargs):
+        if 'origin' in kwargs:
+            if kwargs['origin'] == 'lower':
+                data, data2 = data2, data
+#        data = self._normalize_array(data)
+#        data2 = self._normalize_array(data2)
+        data = np.triu(self.hide_bads(data, col=(0,0,0.5), alpha=1))
+        data2 = np.tril(self.hide_bads(data2, col=(0,0,0.5), alpha=1))
+        plt.imshow(data+data2, *args, **kwargs)
+        self.colorbar = plt.colorbar()
+        self.colorbar.set_label(u'$log_2(N\ of\ reads)$')
+#        data[np.tril_indices_from(data)] = np.ma.masked
+#        plt.imshow(data, *args, **kwargs)
+#        plt.colorbar(orientation='vertical')
+        
+#        data2[np.triu_indices_from(data2)] = np.ma.masked
+#        plt.imshow(data2, *args, **kwargs)
+#        plt.colorbar(orientation='horizontal')    
+    
+    def _normalize_array(self, array):
+        k = np.mean(np.sum(array, axis=1))
+        return array/k
+    
+    def _jaccard(self, data, data2):
+        return (data-data2)/(data+data2)
+    
+    def _chisq(self, data, data2):
+        return (data-data2)**2/(data+data2)
+    
+    def normalize(self, data=None):
+        if data is None:
+            try:
+                self.data = self._normalize_array(self.data)
+            except:
+                print 'No data! Aborting normalization'
+                return
+        else:
+            return self._normalize_array(data)
+        try:
+            self.data2 = self._normalize_array(self.data2)
+        except:
+            print 'No data2, continuing'
+    
+    def make_triangle(self, data=None):
+        if data is None:
+            data=self.data
+        print 'Making triangle'
+        data = np.tril(data)
+        print 'Rotating'
+        data = ndimage.rotate(data, 45, order=0, reshape=True, prefilter=False, 
+                              cval=0)
+        data = self.hide_bads(data, mask_zeroes=False, substitute=0)
+        data = data[~np.all(data == 0, axis=1)]
+        return data
+    
+    def plot_whole_genome_heatmap(self, data=None, data2=None, triangle=False,
+                                  diagonal_markers=False, compare=False, 
+                                  normalize=False, savepath=False, 
+                                  format='svg', colormap=False, *args, **kwargs):
+        if data is None:
+            data=self.data
+        if data2 is None:
+            try:
+                data2 = self.data2
+            except AttributeError:
+                pass
+        if not colormap:
+            colormap=self.cmap
+        else:
+            colormap=cmap.get_cmap(colormap)
         length, height = data.shape
+        if triangle:
+            data = self.make_triangle(data)
+            try:
+                data2 = self.make_triangle(data2)
+            except:
+                pass
+        if normalize:
+            data = self._normalize_array(data)
+            if data2 is not None:
+                data2 = self._normalize_array(data2)
+        self.locations = [np.mean(i)*self.resolution for i in self.boundaries]
+        def determine_aspect(shape, extent):
+            dx = (extent[1] - extent[0]) / float(shape[1])
+            dy = (extent[3] - extent[2]) / float(shape[0])
+            return dx / dy
+        extent = [0, length*self.resolution, 
+                  0, length*self.resolution]
+        aspect = determine_aspect(data.shape, extent)
+        fig = plt.figure(figsize=(15, 10))
+        fig.set_dpi(72)
         self.ax = host_subplot(111)
-        plt.imshow(data, interpolation='none', origin='lower',
-               extent=[0, length*self.resolution, 0, length*self.resolution])
-        bar = plt.colorbar()
-        bar.set_label(label=u'$log_2(число\ чтений)$', size=20)
         self.ax.xaxis.set_tick_params(length=5, direction='out')
         self.ax.yaxis.set_tick_params(length=5, direction='out')
         
@@ -140,26 +253,70 @@ class HiCPlot:
                                           self._nice_ticks))
         self.ax.yaxis.set_major_formatter(mticker.FuncFormatter( \
                                           self._nice_ticks))
-        
-        self.ax.tick_params(axis='x', which='minor', bottom='on')
-        self.ax.tick_params(axis='y', which='minor', left='on')
+#        self.ax.minorticks_on()
+#        self.ax.xaxis.set_minor_locator(MultipleLocator(200000))
+#        self.ax.tick_params(axis='x', which='minor', bottom='on', top='off',
+#                            direction='out', length=2, width=0.01)
+#        self.ax.tick_params(axis='y', which='minor', left='on', right='off',
+#                            direction='out', length=2, width=0.01)
         self.ax.tick_params(axis='x', which='major', bottom='on', top='on', 
-                            labelbottom='on', labelsize=15)
+                            labelbottom='on', labelsize=12)
         self.ax.tick_params(axis='y', which='major', left='on', right='on', 
-                            labelleft='on', labelsize=15)
-        self.ax2 = self.ax.twin()
-        self.ax2.set_xticks(self.locations)
-        self.ax2.set_xticklabels(self.chromosomes, fontsize=20)
-        self.ax2.set_yticks(self.locations)
-        self.ax2.set_yticklabels(self.chromosomes, fontsize=20)
-        self.ax2.xaxis.set_tick_params(length=0)
-        self.ax2.yaxis.set_tick_params(length=0)
-        plt.suptitle(self._get_title(), fontsize=20)
-        self._make_axlabels()
-            
-    def plot_chromosome_pair_heatmap(self, name, name2=None, data=None):
-        if data is None:
-            data=self.data
+                            labelleft='on', labelsize=12)
+        self.ax.set(adjustable='box-forced')
+        self.axChrLabels = self.ax.twin()
+        self.axChrLabels.set_xticks(self.locations)
+        if triangle:
+            self.axChrLabels.yaxis.set_visible(False)
+            self.ax.yaxis.set_visible(True)
+            self.axChrLabels.xaxis.tick_bottom()
+        else:
+            self.axChrLabels.yaxis.tick_right()
+            self.axChrLabels.xaxis.tick_top()
+        self.axChrLabels.set_xticklabels(self.chromosomes, fontsize=15)
+        self.axChrLabels.set_yticks(self.locations)        
+        self.axChrLabels.set_yticklabels(self.chromosomes, fontsize=15)
+        self.axChrLabels.xaxis.set_tick_params(length=0)
+        self.axChrLabels.yaxis.set_tick_params(length=0)
+        if diagonal_markers:
+#            im = plt.imshow(data, visible=False, cmap=colormap, interpolation='none')
+#            self.colorbar = plt.colorbar(im)
+            norm = plt.Normalize(data[np.isfinite(data)].min(), 
+                                 data[np.isfinite(data)].max())
+            data = colormap(norm(data))
+            for multiple in diagonal_markers.keys():
+                for start, end in self.boundaries:
+                    for i in range(start, end, multiple/self.resolution):
+                        data[i, i] = mcolors.ColorConverter().to_rgba(diagonal_markers[multiple])
+        plt.suptitle(self._make_title(), fontsize=15)
+        if not compare:
+            data = self.hide_bads(data, col='blue', alpha=1.0)
+            self.image = plt.imshow(data, interpolation='none', origin='lower',
+                         extent=extent, aspect=aspect, *args, **kwargs)
+            self.barlabel=u'$log_2(N\ of\ reads)$'
+        elif compare == 'Jaccard':
+            self.image = plt.imshow(self._jaccard(data, data2), 
+                                    interpolation='none', origin='lower', 
+                                    extent=extent, cmap='RdBu', *args, **kwargs)
+            self.barlabel=u'Jaccard'
+        elif compare == 'Chi-Square':
+            self.image = plt.imshow(self._chisq(data, data2), 
+                                    interpolation='none', origin='lower', 
+                                    extent=extent, cmap='RdBu', *args, **kwargs)
+            self.barlabel=u'Chi-squared'
+        elif compare == 'Triangles':
+            self._plot_combined_heatmap(data, data2, extent=extent,
+                                        origin='lower', interpolation='none', 
+                                        *args, **kwargs)
+            self._set_axlabels()
+        if compare != 'Triangles':
+#            divider = make_axes_locatable(self.ax)
+#            self.cax = divider.append_axes("right", size="5%", pad=0.05)
+            self.colorbar = plt.colorbar()
+            self.colorbar.set_label(self.barlabel, size=15)
+    
+    def plot_chromosome_pair_heatmap(self, name, name2=None, data=None,
+                                     compare=False, *args, **kwargs):
         n = self.chromosomes.index(name)
         start, end = self.boundaries[n]
         if name2 != None and name2 != name:
@@ -167,7 +324,21 @@ class HiCPlot:
             start2, end2 = self.boundaries[n2]
         else:
             name2, start2, end2 = name, start, end
-        chrdata = data[start:end, start2:end2]
+
+        if data is None:
+            data=self.data
+            chrdata = data[start:end, start2:end2]
+        elif type(data) is list or type(data) is tuple and compare:
+            if len(data) == 2:
+                data, data2 = data
+        else:
+            print 'Something wrong with data!'
+            return ValueError
+        if compare:
+            try:
+                data2=self.data2
+            except:
+                print 'Can not compare, no data2 present!'
         self.ax = host_subplot(111)
         self.ax.xaxis.set_major_formatter(mticker.FuncFormatter( \
                                           self._nice_ticks))
@@ -175,28 +346,49 @@ class HiCPlot:
                                           self._nice_ticks))
         self.ax.xaxis.set_tick_params(top='off', direction='out', length=5)
         self.ax.yaxis.set_tick_params(right='off', direction='out', length=5)
-        plt.imshow(chrdata, origin='lower', interpolation='none',
-                   extent=[0, (end-start)*self.resolution,
-                           0, (end2-start2)*self.resolution])
-        self.bar = plt.colorbar()
-        self.bar.set_label(label=u'$log_2(N\ of\ reads)$', size=20)
+        if not compare:
+            plt.imshow(chrdata, origin='lower', interpolation='none',
+                       extent=[0, (end-start)*self.resolution,
+                               0, (end2-start2)*self.resolution], 
+                       *args, **kwargs)
+            self.bar = plt.colorbar()
+            self.bar.set_label(label=u'$log_2(N\ of\ reads)$', size=15)
+        elif compare == 'Triangles':
+            chrdata = data[start:end, start2:end2]
+            chrdata2 = data2[start:end, start2:end2]
+            self._plot_combined_heatmap(chrdata, chrdata2, origin='lower',
+                                        interpolation='none',
+                                        extent=[0, (end-start)*self.resolution,
+                                            0, (end2-start2)*self.resolution], 
+                                        *args, **kwargs)
+            self._set_axlabels()
+        else:
+            print 'This type of comparison is not implemented for chromosome'\
+                  'pair heatmaps'
+            return
         plt.title(name+'-'+name2)
         
     def plot_by_chromosome_heatmaps(self, data=None, only_intrachromosome=True,
-                                    exclude_names = ('M'),
-                                    savepath=False, format='svg'):
+                                    exclude_names = ('M'), compare=False,
+                                    savepath=False, format='svg', *args, **kwargs):
         if data is None:
             data=self.data
         for chromosome in self.chromosomes:
             if only_intrachromosome:
-                if savepath:
+                self.plot_chromosome_pair_heatmap(chromosome, compare=compare,
+                                                  *args, **kwargs)
+                if savepath:                    
                     filepath = path.join(path.abspath(savepath), 
                                          chromosome+'-'+chromosome+'.'+format)
-                self.plot_chromosome_pair_heatmap(chromosome)
-                self.save_figure(filepath)
+                    self.save_figure(filepath)
+                    self.clear_figure()
+                else:
+                    self.show_figure()
             else:
                 for chromosome1 in self.chromosomes:
-                    self.plot_chromosome_pair_heatmap(chromosome, chromosome1)
+                    self.plot_chromosome_pair_heatmap(chromosome, chromosome1,
+                                                      compare=compare,
+                                                      *args, **kwargs)
                     if savepath:
                         filepath = path.join(path.abspath(savepath), 
                                              chromosome+'-'+chromosome1+'.'+
@@ -205,3 +397,67 @@ class HiCPlot:
                         self.clear_figure()
                     else:
                         self.show_figure()
+                        
+    def _nonzero_stat_by_diagonal(self, data, func=np.mean):
+        """
+        Calculate some statistic (by default, mean) for each diagonal of a 
+        dataset. Return x-values and y-values.
+        """
+        x, y = [], []
+        for d in xrange(1, len(data)):
+            diagonal = np.diagonal(data, d)
+            diagonal = diagonal[np.nonzero(diagonal)]
+            x.append(self.resolution*d)
+            y.append(func(diagonal))
+        return np.array(x), np.array(y)
+        
+    def zero_interchromosomal_interactions(self, data=None):
+        if data is not None:
+            data = self.data
+        for i in self.boundaries:
+            data[i[0]:i[1], 0:i[0]] = 0
+            data[i[0]:i[1], i[1]:-1] = 0
+        return data
+    
+    def scale_plot(self, data=None, plot=True, stat=np.mean, *args, **kwargs):
+        if data is None:
+            data=self.data
+        data[np.isneginf(data)]=0
+        data = self.zero_interchromosomal_interactions(data)
+#        self.ax=plt.subplot(111)
+#        self.ax.imshow(data)
+#        plt.show()
+        x, y = self._nonzero_stat_by_diagonal(data, func=stat)
+        if plot:
+            try:
+                self.ax.plot(x, y, *args, **kwargs)
+            except AttributeError:
+                self.ax=plt.subplot(111)
+                self.ax.plot(x, y, *args, **kwargs)
+            plt.xscale('log')
+            plt.yscale('log')
+        return x, y
+        
+    def fit(self, x, y, func='exp', minx='min', maxx='max', guess=None):
+        from scipy.optimize import curve_fit
+        def exp_func(x, a, b, c):
+            return a * np.exp(-b * x) + c
+            
+        if func == 'exp':
+            func = exp_func
+            if guess is None:
+                guess = (1, 1, 1)
+        else:
+            import inspect
+            n = len(inspect.getargspec(func)['args'])-1
+            guess = [1 for i in xrange(n)]
+            
+        if minx != 'min':
+            x = x[x>minx]
+            y = y[x>minx]
+        if maxx != 'max':
+            x = x[x<maxx]
+            y = y[x<maxx]
+        
+        popt, pcov = curve_fit(func, x, y, guess)
+        return x, exp_func(x, *popt), popt, pcov
--- hicplotlib_test.py
+++ hicplotlib_test.py
@@ -4,19 +4,72 @@ Created on Tue Apr 29 17:53:23 2014
 
 @author: ilya
 """
+#import matplotlib
+
+#matplotlib.use('SVG')
+
 import hicplotlib
-import matplotlib.colors as mcolors
+import numpy as np
+import matplotlib.pyplot as plt
+
 
 a = hicplotlib.HiCPlot()
 a.set_chromosomes(['2L', '2R', '3L', '3R', '4', 'X'])
 a.set_chromosomes_boundaries([(0, 1151), (1151, 2209), (2209, 3437), 
                               (3437, 4833), (4833, 4901), (4901, 6023)])
 a.set_resolution(20000)
-a.read_and_combine_two_matrices('/media/ilya/SSD/HiC/S2/IC-heatmap-20K.mtx',
-'/media/ilya/SSD/HiC/S2LD/IC-heatmap-20K.mtx', ('S2', 'S2LD'))
-c = mcolors.ColorConverter().to_rgb
-a.make_cmap([c('white'), c('yellow'),
-     0.4, c('yellow'), c('red'),
-     0.8, c('red'), c('black')])
-a.apply_cmap()
-a.plot_by_chromosome_heatmaps(savepath='/home/ilya/Документы/biology/Drosophila_cells_Hi-C/HiC_results/heatmaps/S2LD/20K/S2vsS2LD')
+#a.read_two_matrices(('/media/ilya/SSD/HiC/S2/IC-heatmap-20K.mtx', 
+#                     '/media/ilya/SSD/HiC/S2LD/IC-heatmap-20K.mtx'), 
+#                    ('S2', 'S2LD'))
+a.read_matrix('/home/ilya/Документы/biology/Drosophila_cells_Hi-C/BG3/IC-heatmap-20K.mtx', 'BG3')
+a.plot_whole_genome_heatmap(colormap='jet', 
+                            diagonal_markers={10**5:'grey', 10**6:'black'})
+#a.hide_bads()
+#c = mcolors.ColorConverter().to_rgb
+#a.make_cmap(
+#    [c('white'), c('yellow'),
+#     0.4, c('yellow'), c('red'),
+#     0.8, c('red'), c('black')])
+#a.apply_cmap()
+#a.data = a.data/np.sum(a.data)
+#a.data2 = a.data2/np.sum(a.data2)
+#data = (a.data-a.data2)/(a.data+a.data2)
+#absmax = np.max(np.abs(data))
+#print data[0:100, 0:100]
+#a.data = a._normalize_array(a.data)
+#a.data2 = a._normalize_array(a.data2)
+#data = np.tril(a.data)+np.triu(a.data2)
+#data = (a.data-a.data2)/(a.data+a.data2)
+#a.plot_by_chromosome_heatmaps(compare='Triangles', savepath='/home/ilya/Документы/biology/Drosophila_cells_Hi-C/HiC_results/heatmaps/BG3vsOSC/', format='svg', vmin=0)#, vmax=absmax)
+#x1, y1 = a.scale_plot(a.data, color='g', plot=True, label='S2')#, stat=np.median)
+#x2, y2 = a.scale_plot(a.data2, color='b', plot=True, label='S2LD')#, stat=np.median)
+
+#s2x, s2y, s2opt, s2cov = a.fit(x1, y1, minx=5*10**5, maxx=3*10**6)
+#print 'a*e^(-b*x)+c'
+#print 'S2 fit:', s2opt
+##print zip(s2xfit, s2yfit)
+#s2ldx, s2ldy, s2ldopt, s2ldcov = a.fit(x2, y2, minx=5*10**5, maxx=3*10**6)
+#print 'S2LD fit:', s2ldopt
+##print zip(s2ldxfit, s2ldyfit)
+#
+#plt.plot(s2x, s2y, 'g--', label='S2 exponential fit')
+#plt.plot(s2ldx, s2ldy, 'b--', label='S2LD exponential fit')
+#def exp_func(x, a, b, c):
+#    return a * np.exp(-b * x) + c
+    
+#yfit = exp_func(x1, 1, 1, 1)
+#plt.plot(x1, yfit, 'g--', label=)
+
+
+#plt.legend()
+#plt.xlim(4*10**4, 10**7)
+#plt.ylim(10**-3, 20)
+#plt.title('Scale plot')
+#plt.grid()
+#plt.xlabel('Distance (bp)')
+#plt.ylabel('Contact probability')
+#a.show_figure()
+#a._set_axlabels()
+#a.ax.locator_params(nbins=15)
+#a.show_figure()
+a.save_figure(savepath='/home/ilya/Документы/biology/Drosophila_cells_Hi-C/HiC_results/heatmaps/BG3/all-all.svg', format='svg')
