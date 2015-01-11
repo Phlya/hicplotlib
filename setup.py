ff68e8bd774431c54081344912b5a655b70b2d0f
Ilya Flyamer <flyamer@gmail.com>
2015-01-12 00:39:29 +0300

    Now installable with distutils and fixed a bug in settings.calculate_boundaries()

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='hicplotlib',
+      version='0.1',
+      description='Hi-C data plotting and analysis tool',
+      author='Ilya Flyamer',
+      author_email='flyamer@gmail.com',
+      url='https://github.com/Phlya/hicplotlib',
+      packages=['hicplotlib'],
+     )
